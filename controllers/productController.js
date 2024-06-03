86469b735ab4dc522f87f95484941881fc12d2cb
ohziiilov3 <klbaskerville0520@gmail.com>
2024-06-03 10:47:30 -0700

    Milestone 1: CRUD for Product Model complete

--- controllers/productController.js
+++ controllers/productController.js
@@ -25,12 +25,54 @@ try{
 }catch(error){
     res.status(500).json({error: error.message})
 }
+};
+
+
+const getProductById = async (req,res)=>{
+    const {id} = req.params;
+    try {
+     const product = await prisma.product.findUnique({
+        where: {id: parseInt(id)},
+     });
+     res.json(product);
+    } catch (error) {
+        res.staus(500).json({error:message})
+    }
 }
 
+
+const updateProduct = async (req,res) =>{
+const { id } = req.params;
+const {name, description, price, image_url, category} = req.body;
+try {
+  const product = await prisma.product.update({
+    where: { id: parseInt(id) },
+    data: { name, description, price, image_url, category },
+  });
+  res.json(product);
+} catch (error) {
+  res.status(500).json({ error: message });
+}
+}
+
+const deleteProduct = async (req, res)=>{
+     const { id } = req.params;
+     try {
+       const product = await prisma.product.delete({
+         where: { id: parseInt(id) },
+       });
+       res.json(product);
+     } catch (error) {
+       res.staus(500).json({ error: message });
+     }
+}
 
 
 
 module.exports = {
     createProduct,
     getProducts,
+    getProductById,
+    updateProduct,
+    deleteProduct,
 }
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,9 +3,12 @@ const router = express.Router();
 const productController = require('../controllers/productController');
 
 
-
+// Routes -> /api/products
 router.post('/products', productController.createProduct);
 router.get('/products', productController.getProducts);
+router.get('/products/:id', productController.getProductById);
+router.put('/products/:id', productController.updateProduct);
+router.delete('/products/:id', productController.deleteProduct);
 
 
 
